c7737eb11f385499923200555d31c102a6d2b2e8
Reorx <novoreorx@gmail.com>
2013-04-14 20:26:38 +0800

    add example/color.py

---
+++ examples/color.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from terminal import color
+
+text = u'Lorem 中 Ipsum 文'.encode(sys.stdout.encoding)
+
+print '\nColors:\n'
+for color_name in color._colors:
+    print getattr(color, color_name)(text), ' <- ' + color_name
+
+print '\n\nStyles:\n'
+for style_name in color._styles:
+    print getattr(color, style_name)(text), ' <- ' + style_name
+
+print '\n\nStyles(Colors):\n'
+for style_name in color._styles:
+    style_func = getattr(color, style_name)
+    for color_name in color._colors:
+        color_func = getattr(color, color_name)
+        print style_func(color_func(text)), ' <- %s(%s)' % (style_name, color_name)
+
+print '\n\nColors(Styles):\n'
+for color_name in color._colors:
+    color_func = getattr(color, color_name)
+    for style_name in color._styles:
+        style_func = getattr(color, style_name)
+        print color_func(style_func(text)), ' <- %s(%s)' % (color_name, style_name)
